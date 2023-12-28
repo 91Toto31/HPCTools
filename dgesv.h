a638d03d976044411a20ef34f806e350a914e1b6
91Toto31 <146136703+91Toto31@users.noreply.github.com>
2023-12-28 10:57:09 +0100

    Update dgesv.h

--- dgesv.h
+++ dgesv.h
@@ -2,10 +2,13 @@
 #ifndef DGESV_H
 #define DGESV_H
 
-void eliminationGauss(double matrice[N][N + 1], int N);
-void substitutionArriere(double matrice[N][N + 1], double solutions[N], int N);
-int my_dgesv(int n, int nrhs, double *a, double *b, int N);
+extern const int N;
+
+void eliminationGauss(double matrice[N][N + 1]);
+void substitutionArriere(double matrice[N][N + 1], double solutions[N]);
+int my_dgesv(int n, int nrhs, double *a, double *b);
 
 #endif
+
 
 
