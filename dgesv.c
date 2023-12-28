a8f87c8150363e3a00167354983295161a8f945d
91Toto31 <146136703+91Toto31@users.noreply.github.com>
2023-12-28 11:43:18 +0100

    Update dgesv.c

--- dgesv.c
+++ dgesv.c
@@ -2,34 +2,33 @@
 #include "dgesv.h"
 #include <math.h>
 
-const int N = 3;
-
-void printMatrix(const char* name, double matrix[N][N + 1]) {
-    printf("%s:\n", name);
+void afficherMatrice(double matrice[N][N + 1]) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N + 1; j++) {
-            printf("%.2f\t", matrix[i][j]);
+            printf("%.2f\t", matrice[i][j]);
         }
         printf("\n");
     }
-    printf("\n");
 }
 
 void eliminationGauss(double matrice[N][N + 1]) {
     for (int col = 0; col < N - 1; col++) {
+        // Recherche du pivot maximal dans la colonne actuelle
         int max_row = col;
         for (int i = col + 1; i < N; i++) {
-            if (fabs(matrice[i][col]) > fabs(matrice[max_row][col])) {
+            if (fabs(matrice[i][col]) > fabs(matrice[max_row][col])) { //"fabs" permet de calculer la valeur absolue d'un nombre en virgule flottante en C.
                 max_row = i;
             }
         }
+        // Échange des lignes pour mettre le pivot maximal en haut, pour avoir une plus grande stabilité
         for (int j = 0; j < N + 1; j++) {
-            double tempon = matrice[col][j];
+            double tempon = matrice[col][j]; 
             matrice[col][j] = matrice[max_row][j];
             matrice[max_row][j] = tempon;
         }
+        // Échelonnement des lignes
         for (int i = col + 1; i < N; i++) {
-            double factor = matrice[i][col] / matrice[col][col];
+            double factor = matrice[i][col] / matrice[col][col]; //en gros permet d'échelonné avec la diagonale de la matrice (dur à expliquer)
             for (int j = col; j < N + 1; j++) {
                 matrice[i][j] -= factor * matrice[col][j];
             }
@@ -37,6 +36,7 @@ void eliminationGauss(double matrice[N][N + 1]) {
     }
 }
 
+//résolution du système du bas vers le haut
 void substitutionArriere(double matrice[N][N + 1], double solutions[N]) {
     for (int i = N - 1; i >= 0; i--) {
         solutions[i] = matrice[i][N];
