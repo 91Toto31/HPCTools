f6c5c014fc0e133041a7cb0b3676623b96d80723
91Toto31 <146136703+91Toto31@users.noreply.github.com>
2023-12-28 10:42:48 +0100

    Update dgesv.h

--- dgesv.h
+++ dgesv.h
@@ -1,1 +1,8 @@
-int my_dgesv(int n, int nrhs, double *a, double *b /* add/change the arguments according to your implementation needs */);
+int // dgesv.h
+#ifndef DGESV_H
+#define DGESV_H
+
+int my_dgesv(int n, int nrhs, double *a, double *b);
+
+#endif
+
